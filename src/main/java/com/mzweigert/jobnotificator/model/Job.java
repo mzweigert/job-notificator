6aba9990635f47fa43ceeedcdd227adf6e22427f
mzweigert <mateuszzweigert@gmail.com>
2019-09-02 22:01:24 +0200

    Make basic database structure entities.

---
+++ src/main/java/com/mzweigert/jobnotificator/model/Job.java
@@ -0,0 +1,31 @@
+package com.mzweigert.jobnotificator.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Job extends IdentifiableEntity {
+
+    @OneToOne(optional = false)
+    private SourcePage sourcePage;
+
+    @Column(nullable = false)
+    private String url;
+
+    public SourcePage getSourcePage() {
+        return sourcePage;
+    }
+
+    public void setSourcePage(SourcePage sourcePage) {
+        this.sourcePage = sourcePage;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
---
+++ src/main/java/com/mzweigert/jobnotificator/model/Receiver.java
@@ -0,0 +1,60 @@
+package com.mzweigert.jobnotificator.model;
+
+import javax.persistence.*;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Entity
+public class Receiver extends ConfigurableEntity {
+
+    @Column(nullable = false)
+    private String mail;
+
+    @ManyToMany
+    @JoinTable(
+            name = "SentJobsToReceiver",
+            joinColumns = {
+                    @JoinColumn(name = "jobId", referencedColumnName = "id", foreignKey = @ForeignKey(name = "FK_SentJobsToReceiver_Job"))
+            },
+            inverseJoinColumns = {
+                    @JoinColumn(name = "receiverId", referencedColumnName = "id", foreignKey = @ForeignKey(name = "FK_SentJobsToReceiver_Receiver"))
+            })
+    private Set<Job> sentJobs = new LinkedHashSet<>();
+
+    @ManyToMany
+    @JoinTable(
+            name = "SubscribedSourcePagesReceiver",
+            joinColumns = {
+                    @JoinColumn(name = "sourcePageId", referencedColumnName = "id",
+                            foreignKey = @ForeignKey(name = "FK_SubscribedSourcePagesReceiver_SourcePage"))
+            },
+            inverseJoinColumns = {
+                    @JoinColumn(name = "receiverId", referencedColumnName = "id",
+                            foreignKey = @ForeignKey(name = "FK_SubscribedSourcePagesReceiver_Receiver"))
+            })
+    private Set<SourcePage> subscribedSourcePages = new LinkedHashSet<>();
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public Set<Job> getSentJobs() {
+        return sentJobs;
+    }
+
+    public void setSentJobs(Set<Job> sentJobs) {
+        this.sentJobs = sentJobs;
+    }
+
+    public Set<SourcePage> getSubscribedSourcePages() {
+        return subscribedSourcePages;
+    }
+
+    public void setSubscribedSourcePages(Set<SourcePage> subscribedSourcePages) {
+        this.subscribedSourcePages = subscribedSourcePages;
+    }
+}
